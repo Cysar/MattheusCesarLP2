7a4900c19c708baf70d0cef528f2233b8b25c14a
mattheus <shablau11@gmail.com>
2017-06-13 15:34:23 -0300

    agr foi rapaziada

--- Soluções/Competiçao-de-Corrida/Competiçao-de-Corrida/Properties/AssemblyInfo.cs
+++ Soluções/Competiçao-de-Corrida/Competiçao-de-Corrida/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
+[assembly: AssemblyTitle("Competiçao-de-Corrida")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hewlett-Packard Company")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyProduct("Competiçao-de-Corrida")]
 [assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("317a8d8a-a534-4e82-aa62-4cb5e389197d")]
+[assembly: Guid("4977b1b8-b14d-4ce5-bf50-9b60e5f25806")]
 
 // Version information for an assembly consists of the following four values:
 //
