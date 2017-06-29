fcaec9ed33a5431377be927b24dc0a6f036404df
jairo151099 <jairojunior10410@gmail.com>
2017-06-29 14:31:52 -0300

    vrau

---
+++ Soluções/prova/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Questao
+    {
+        public int tomA, tomB, tomC, tomD, tomE, respn, respinv;
+        public string resp;
+        public void Resposta()
+        {
+            respn = 0;
+            respinv = 0;
+
+            if (tomA <= 100)
+            {
+                resp = "A";
+                respn++;
+            }
+            else if (tomA <= 155)
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Questao
+    {
+        public int tomA, tomB, tomC, tomD, tomE, respn, respinv;
+        public string resp;
+        public void Resposta()
+        {
+            respn = 0;
+            respinv = 0;
+
+                if (tomA <= 100)
+                {
+                    resp = "A";
+                    respn++;
+                }
+                else if (tomA <= 155)
+                    respinv++;
+
+                if (tomB <= 100)
+                {
+                    resp = "B";
+                    respn++;
+                }
+                else if (tomB <= 155)
+                    respinv++;
+                if (tomC <= 100)
+                {
+                    resp = "C";
+                    respn++;
+                }
+                else if (tomC <= 155)
+                    respinv++;
+                if (tomD <= 100)
+                {
+                    resp = "D";
+                    respn++;
+                }
+                else if (tomD <= 155)
+                    respinv++; 
+                if (tomE <= 100)
+                {
+                    resp = "E";
+                    respn++;
+                }
+                else if (tomE <= 155)
+                    respinv++;
+            
+            if (respn != 1 || respinv != 0)
+                Console.WriteLine("*");
+            else
+                Console.WriteLine(resp);
+
+    }
+    }
+}
+
+
+
---
+++ Soluções/prova/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, n = int.Parse(Console.ReadLine());
+            Questao[] ex = new Questao[n];
+            i = 0;
+            while (i < n)
+            {
+                ex[i] = new Questao();
+
+                ex[i].tomA = int.Parse(Console.ReadLine());
+                ex[i].tomB = int.Parse(Console.ReadLine());
+                ex[i].tomC = int.Parse(Console.ReadLine());
+                ex[i].tomD = int.Parse(Console.ReadLine());
+                ex[i].tomE = int.Parse(Console.ReadLine());
+
+                i++;
+            }
+            i = 0;
+            while (i < n)
+            {
+                ex[i].Resposta();
+                i++;
+            }
+        }
+    }
+}
