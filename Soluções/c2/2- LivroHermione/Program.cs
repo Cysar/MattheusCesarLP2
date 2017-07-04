20660ad485602e60ee4e53a2c275df844f1e100d
Mattheus Cesar <shablau11@gmail.com>
2017-07-04 15:56:43 -0300

    hagnarock

---
+++ Soluções/c2/2- LivroHermione/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int pph, hpd, npg = int.Parse(Console.ReadLine()), i;
+            Livro[i] book = new Livro();
+        }
+    }
+}
