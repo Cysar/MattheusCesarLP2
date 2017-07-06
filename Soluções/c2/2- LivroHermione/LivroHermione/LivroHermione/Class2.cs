e2108447876ac70cc0a86b38f8f459a33d13ad5b
mattheus <shablau11@gmail.com>
2017-07-06 14:29:41 -0300

    lol

---
+++ Soluções/c2/2- LivroHermione/LivroHermione/LivroHermione/Class2.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livro
+{
+    class Livro
+    {
+        public string titulo = Console.ReadLine();
+        public double qtdpg = double.Parse(Console.ReadLine()), hpd = double.Parse(Console.ReadLine()), pph = double.Parse(Console.ReadLine());
+
+        public double TempodLeitura(int pph)
+        {
+            return qtdpg / pph;
+        }
+
+    }
+}
