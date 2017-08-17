a0de9fa1be031d4863ff82b84777a798d9c64e02
mattheus cesar <shablau11@gmail.com>
2017-08-17 14:06:49 -0300

    oi

---
+++ Soluções/c2/2- LivroHermione/Aula10-08-17/Aula10-08-17/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Aula10_08_17
+{
+    /// <summary>
+    /// Interação lógica para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
